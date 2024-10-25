00b42b0757f9897c79d5c240156648466a7bd4db
Stuart Forrester <stforrester@gmail.com>
2024-10-25 10:10:30 -0500

    complete the lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Stuart is the champion";
+
+document.body.append(newHeader);
